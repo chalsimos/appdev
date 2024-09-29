872ddce79e904de6c1d2c92466c0f4d9f3a06d33
chalsimos <krizchan31@gmail.com>
2024-09-29 13:33:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) =>{
+    console.log('hello world');
+    
+});
+
+app.listen(3000, ()=>{
+    console.log('server running on http://localhost:3000/');
+    
+});
